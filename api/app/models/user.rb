aac43aaf62c5b08594520a7a4ba17832fac2603b
missi0415 <missi0415@gmail.com>
2021-07-09 19:10:20 +0900

    Userモデルバリデーション

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+
+  # gem bcrypt のメソッド
+  # password_digestというカラムに保存されるため、同一名称のカラムを用意しておく
+  has_secure_password
+
+  validates :name,  presence: true,
+                    length: { maximum: 30,allow_blank: true}
+
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+  validates :password, presence: true,
+                        length: { minimum: 8 },
+                        format: {
+                          with: VALID_PASSWORD_REGEX
+                        },
+                        allow_nil: true
+  validates :activated, inclusion: { in: [ true, false] }
 end
