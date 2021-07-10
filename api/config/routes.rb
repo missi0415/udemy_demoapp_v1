bd055fdf1169c99f596049d1328fb42da05b9bd2
missi0415 <missi0415@gmail.com>
2021-07-10 23:17:13 +0900

    Nuxtへの表示

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     # 「controllers」以下のディレクトリ階層を表します。
     namespace :v1 do
-      #api test action
-      resources :hello, only:[:index]
-      # ルートはhttp://localhost:3000/api/v1/hello
+      resources :users, only:[:index]
     end
   end
 end
