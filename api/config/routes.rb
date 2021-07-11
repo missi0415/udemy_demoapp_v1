2daa26e2c7490dd75db7785bbb710c8731f8173e
missi0415 <missi0415@gmail.com>
2021-07-11 19:15:31 +0900

    vuetifyの導入

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    # 「controllers」以下のディレクトリ階層を表します。
     namespace :v1 do
+      # 追加
       resources :users, only:[:index]
+      # 削除
+      # resources :hello, only:[:index]
     end
   end
 end
